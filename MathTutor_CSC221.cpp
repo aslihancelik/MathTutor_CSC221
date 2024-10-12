983cc2f47e5bc90973471f7cb3a50af7b66ffe37
aslihancelik <aslihancelik27@gmail.com>
2024-10-12 18:52:43 -0400

    revision added, now the user can enter an input

--- MathTutor_CSC221.cpp
+++ MathTutor_CSC221.cpp
@@ -1,7 +1,8 @@
 // MathTutor_CSC221.cpp : This file contains sums two numbers provided by the user 
 // while giving them the time they need to make the calculation and check their
 // answer once they are ready to do so. The program outputs the sum of two numbers.
-//
+// Program also tells the user if their answer is correct or not before showing the
+// answer by allowing th user to input their finding.
 //
 // Programmer: Aslihan Celik
 // Datew: 10/12/2024
@@ -36,7 +37,7 @@ void printSummation(int maxWidth, int number1, int number2) {
 
 int main()
 {
-    int number1, number2, sum;
+    int number1, number2, sum, userAnswer;
 
     cout << "Please enter the first value you would like to use in summation:" << endl;
     cin >> number1;
@@ -49,17 +50,26 @@ int main()
 
     cout << "The summation of the two numbers you entered will look like follows:" << endl;
     printSummation(maxWidth, number1, number2);
- 
-   
+    
+    //sum the two numbers
+    sum = number1 + number2;
+
     // Pause to allow student time to solve
-    cout << "When you are ready, hit enter to see the answer..." << endl;
+    cout << "Please enter your answer: " << endl;
     cin.ignore();  // To ignore any newline left from previous inputs
-    cin.get();     // Wait for the user to press a key
+    cin >> userAnswer;     // Wait for the user to press a key
+    cout << endl;
+
+    // Check if the user's answer is correct and display feedback
+    if (userAnswer == sum) {
+        cout << "You are correct!" << endl << endl;
+    }
+    else {
+        cout << "Your answer is not correct." << endl << endl;
+    }
 
     // Show the correct solution
     cout << "The answer is: " << endl << endl;
-    //sum the two numbers
-    sum = number1 + number2;
     printSummation(maxWidth, number1, number2);
 
     //output the sum of the two numbers
